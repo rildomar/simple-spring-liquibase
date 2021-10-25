3db35a82a7c968e761f0ba1277b7cd24fec572ad
rildomar.oliveira <rildomar.oliveira@cwi.com.br>
2021-10-25 15:37:00 -0300

    Initial Config

---
+++ src/main/java/br/com/simplespring/demo/model/UserModel.java
@@ -0,0 +1,18 @@
+package br.com.simplespring.demo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class UserModel {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+}
